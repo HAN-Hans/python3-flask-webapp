8894f880dfd51446e70d7866b89b7c5bad063e02
hongweihan <job.han23@gmail.com>
2018-06-10 17:27:31 +0800

    Fix bug

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from flask import current_app
 from flask_login import UserMixin
-from itsdangerous import Serializer
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
